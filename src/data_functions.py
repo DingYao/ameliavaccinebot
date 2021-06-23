3c1026770457d801d7e8bc5ba3957e3d395c95b7
DingYao <DYWAN@live.com.sg>
2021-06-23 04:39:32 -0400

    Format status and reschedule responses

--- src/data_functions.py
+++ src/data_functions.py
@@ -121,13 +121,13 @@ def processResponseDict(responseDict):
         appointmentStatus = appointment["status"]
         if appointment["status"] == '':
             appointmentStatus += 'not completed'
-        returnText += (f'*Appointment {appointmentCount}*\n'
-                       f'_{appointment["location"]["name"]}_\n'
-                       f'bookingId: _{appointment["id"]}_\n'
-                       f'slotId: _{appointment["slot_id"]}_\n'
-                       f'address:\n_{appointment["location"]["address"]}_\n'
-                       f'time: _{appointment["time"]}_\n'
-                       f'status: _{appointmentStatus}_\n\n'
+        returnText += (f'*Appointment {appointmentCount}\n'
+                       f'{appointment["location"]["name"]}*\n'
+                       f'*bookingId:* _{appointment["id"]}_\n'
+                       f'*slotId:* _{appointment["slot_id"]}_\n'
+                       f'*address:*\n_{appointment["location"]["address"]}_\n'
+                       f'*time:* _{appointment["time"]}_\n'
+                       f'*status:* _{appointmentStatus}_\n\n'
                        )
     return returnText
 
